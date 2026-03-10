40654099b9d793306c511fb9d50cc5fdc1a946ce
Pippo <enzopippo230@gmail.com>
2026-03-10 19:58:20 -0300

    Page dos politicos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { AppRoutes } from '../src/routes/AppRoutes';
 export default function App() {
   return (
     <BrowserRouter>
-      <main className="min-h-screen flex flex-col bg-brand-darkest">
+      <main className="flex w-full min-h-screen bg-gray-100">
         <AppRoutes />
       </main>
     </BrowserRouter>
--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,26 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import type { Deputado } from '../models/deputado';
+import { PoliticosPage } from '../page/politicosPage';
 
 export function AppRoutes() {
-  const deputado: Deputado = {
-    id: 1,
-    idApi: 204554,
-    idLegislatura: 57,
-    nomeUrna: 'João Silva',
-    nomeCompleto: 'João da Silva',
-    cargo: 'Deputado Federal',
-    estadoId: 25,
-    partidoId: 10,
-    email: 'joao@camara.leg.br',
-    fotoUrl: 'https://www.camara.leg.br/internet/deputado/bandep/220593.jpg',
-    ativo: true,
-    atualizadoEm: null,
-  };
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
 
-      {/* <Route path="/estados" element={<EstadosPage />} /> */}
-    </Routes>
+        <Route path="/politicos" element={<PoliticosPage />} />
+      </Routes>
   );
 }
