5066d2b4a96a674d3f0adb84a8247c12e0120ad7
Pippo <enzopippo230@gmail.com>
2026-03-11 21:35:32 -0300

    Realizado um placeholder da Homepage

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import { HomePage } from '../pages/homePage';
 import { PoliticosPage } from '../pages/politicosPage';
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-
+      <Route path="/" element={<HomePage />} />
       <Route path="/politicos" element={<PoliticosPage />} />
     </Routes>
   );
